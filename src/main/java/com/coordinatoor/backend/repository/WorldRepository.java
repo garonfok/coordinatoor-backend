c42262992fedfefb41f0e2a50862782e6624e048
Garon Fok <fokgaron@gmail.com>
2024-02-01 08:26:20 +0000

    fix: add repository annotation

--- src/main/java/com/coordinatoor/backend/repository/WorldRepository.java
+++ src/main/java/com/coordinatoor/backend/repository/WorldRepository.java
@@ -3,9 +3,11 @@ package com.coordinatoor.backend.repository;
 import java.util.List;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.coordinatoor.backend.model.World;
 
+@Repository
 public interface WorldRepository extends ListCrudRepository<World, Long> {
   List<World> findByName(String name);
 }
