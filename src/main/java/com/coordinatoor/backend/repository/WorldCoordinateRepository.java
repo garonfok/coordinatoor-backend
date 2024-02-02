d67b3ad480e291478fb56d10a220d6bced4717f9
Garon Fok <fokgaron@gmail.com>
2024-02-02 09:00:10 +0000

    feat: add repository for worldcoordinate

--- src/main/java/com/coordinatoor/backend/repository/WorldCoordinateRepository.java
+++ src/main/java/com/coordinatoor/backend/repository/WorldCoordinateRepository.java
@@ -1,10 +1,19 @@
 package com.coordinatoor.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.coordinatoor.backend.entity.World;
 import com.coordinatoor.backend.entity.WorldCoordinate;
+import com.coordinatoor.backend.entity.WorldCoordinate.DimensionEnum;
 
 @Repository
 public interface WorldCoordinateRepository extends ListCrudRepository<WorldCoordinate, Long> {
+
+  public List<WorldCoordinate> findAllByWorldOrderByName(World world);
+
+  public List<WorldCoordinate> findAllByWorldAndDimensionOrderByName(World world, DimensionEnum dimension);
+
 }
