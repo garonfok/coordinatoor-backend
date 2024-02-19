d39818cdd654a4419e5858a05b13b2f5bee2983c
Garon Fok <fokgaron@gmail.com>
2024-02-19 09:46:24 +0000

    feat: add update method

--- src/main/java/com/coordinatoor/backend/controller/ProfileController.java
+++ src/main/java/com/coordinatoor/backend/controller/ProfileController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,32 +23,45 @@ public class ProfileController {
   ProfileRepository profileRepository;
 
   @GetMapping(path = "/{id}", produces = "application/json")
-  public Profile getProfile(@PathVariable("id") Long id) {
+  public Profile getProfile(@PathVariable Long id) {
     return profileRepository.findById(id).orElse(null);
   }
 
   @GetMapping(path = "/email/{email}", produces = "application/json")
-  public Profile getProfileByEmail(@PathVariable("email") String email) {
+  public Profile getProfileByEmail(@PathVariable String email) {
     return profileRepository.findByEmail(email).orElse(null);
   }
 
   @GetMapping(path = "/search/email/{email}", produces = "application/json")
-  public List<Profile> getProfilesByEmail(@PathVariable("email") String email) {
+  public List<Profile> getProfilesByEmail(@PathVariable String email) {
     return profileRepository.findByEmailContains(email);
   }
 
   @GetMapping(path = "/search/username/{username}", produces = "application/json")
-  public List<Profile> getProfilesByUsername(@PathVariable("username") String username) {
+  public List<Profile> getProfilesByUsername(@PathVariable String username) {
     return profileRepository.findByUsernameContains(username);
   }
 
   @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
-  public Profile createProfile(Profile profile) {
+  public Profile createProfile(@RequestBody Profile profile) {
     return profileRepository.save(profile);
   }
 
+  @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+  public Profile updateProfile(@PathVariable Long id, @RequestBody Profile newProfile) {
+    return profileRepository.findById(id).map(
+        profile -> {
+          profile.setEmail(newProfile.getEmail());
+          profile.setUsername(newProfile.getUsername());
+          return profileRepository.save(profile);
+        }).orElseGet(() -> {
+          newProfile.setId(id);
+          return profileRepository.save(newProfile);
+        });
+  }
+
   @DeleteMapping(path = "/{id}")
-  public void deleteProfile(@PathVariable("id") Long id) {
+  public void deleteProfile(@PathVariable Long id) {
     profileRepository.deleteById(id);
   }
 }
--- src/main/java/com/coordinatoor/backend/controller/WorldController.java
+++ src/main/java/com/coordinatoor/backend/controller/WorldController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coordinatoor.backend.entity.Profile;
 import com.coordinatoor.backend.entity.World;
+import com.coordinatoor.backend.repository.ProfileRepository;
 import com.coordinatoor.backend.repository.WorldRepository;
 
 @RestController
@@ -19,23 +24,45 @@ public class WorldController {
   @Autowired
   WorldRepository worldRepository;
 
+  @Autowired
+  ProfileRepository profileRepository;
+
   @GetMapping(path = "/{id}", produces = "application/json")
-  public World getWorld(Long id) {
+  public World getWorld(@PathVariable Long id) {
     return worldRepository.findById(id).orElse(null);
   }
 
   @GetMapping(path = "/search/name/{name}", produces = "application/json")
-  public List<World> getWorldByName(String name) {
+  public List<World> getWorldByName(@PathVariable String name) {
     return worldRepository.findByNameContains(name);
   }
 
+  @GetMapping(path = "/user/{id}/owner", produces = "application/json")
+  public Profile getWorldOwnerProfile(@PathVariable Long id) {
+    return profileRepository.findByWorldOwner(id);
+  }
+
   @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
-  public World createWorld(World world) {
+  public World createWorld(@RequestBody World world) {
     return worldRepository.save(world);
   }
 
+  @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+  public World updateWorld(@PathVariable Long id, @RequestBody World newWorld) {
+    return worldRepository.findById(id).map(
+        world -> {
+          world.setName(newWorld.getName());
+          world.setSeed(newWorld.getSeed());
+          world.setIpAddress(newWorld.getIpAddress());
+          return worldRepository.save(world);
+        }).orElseGet(() -> {
+          newWorld.setId(id);
+          return worldRepository.save(newWorld);
+        });
+  }
+
   @DeleteMapping(path = "/{id}")
-  public void deleteWorld(Long id) {
+  public void deleteWorld(@PathVariable Long id) {
     worldRepository.deleteById(id);
   }
 
