3c48343ab09a78e65c5101823f3f87ec0abd838f
Garon Fok <fokgaron@gmail.com>
2024-02-24 04:31:24 +0000

    refactor: change post/put request bodys to pojo

--- src/test/java/com/coordinatoor/backend/ControllerTests.java
+++ src/test/java/com/coordinatoor/backend/ControllerTests.java
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.coordinatoor.backend.entity.Profile;
 import com.coordinatoor.backend.repository.ProfileRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -26,70 +27,82 @@ import com.coordinatoor.backend.repository.ProfileRepository;
 @AutoConfigureMockMvc
 public class ControllerTests {
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Autowired
-  private ProfileRepository profileRepository;
+    @Autowired
+    private ProfileRepository profileRepository;
 
-  @BeforeAll
-  public void setup() {
-    profileRepository.save(new Profile("johnsmith", "johnsmith@email.com"));
-  }
+    @BeforeAll
+    public void setup() {
+        profileRepository.save(new Profile("johnsmith", "johnsmith@email.com"));
+    }
 
-  @Test
-  public void testProfile() throws Exception {
-    mockMvc.perform(get("/profile/1"))
-        .andExpect(status().isOk())
-        .andExpect(
-            jsonPath("$.username").value("johnsmith"))
-        .andExpect(jsonPath("$.email").value("johnsmith@email.com"));
-    mockMvc.perform(get("/profile/email/johnsmith@email.com"))
-        .andExpect(status().isOk()).andExpect(
-            jsonPath("$.username").value("johnsmith"))
-        .andExpect(jsonPath("$.email").value("johnsmith@email.com"));
-    ;
+    @Test
+    public void testProfile() throws Exception {
+        mockMvc.perform(get("/profile/1"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        jsonPath("$.username").value("johnsmith"))
+                .andExpect(jsonPath("$.email").value("johnsmith@email.com"));
+        mockMvc.perform(get("/profile/email/johnsmith@email.com"))
+                .andExpect(status().isOk()).andExpect(
+                        jsonPath("$.username").value("johnsmith"))
+                .andExpect(jsonPath("$.email").value("johnsmith@email.com"));
+        ;
 
-    mockMvc.perform(get("/profile/search/email/johnsmith"))
-        .andExpect(status().isOk())
-        .andExpect(
-            jsonPath("$[0].username").value("johnsmith"))
-        .andExpect(jsonPath("$[0].email").value("johnsmith@email.com"));
+        mockMvc.perform(get("/profile/search/email/johnsmith"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        jsonPath("$[0].username").value("johnsmith"))
+                .andExpect(jsonPath("$[0].email").value("johnsmith@email.com"));
 
-    mockMvc.perform(get("/profile/search/username/john"))
-        .andExpect(status().isOk())
-        .andExpect(
-            jsonPath("$[0].username").value("johnsmith"))
-        .andExpect(jsonPath("$[0].email").value("johnsmith@email.com"));
+        mockMvc.perform(get("/profile/search/username/john"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        jsonPath("$[0].username").value("johnsmith"))
+                .andExpect(jsonPath("$[0].email").value("johnsmith@email.com"));
 
-    mockMvc.perform(post("/profile/")
-        .contentType("application/json")
-        .content("{\"username\":\"johndoe\",\"email\":\"johndoe@email.com"
-            + "\"}"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.username").value("johndoe"))
-        .andExpect(jsonPath("$.email").value("johndoe@email.com"));
+        Profile postProfile = new Profile("johndoe", "johndoe@email.com");
 
-    mockMvc.perform(get("/profile/2"))
-        .andExpect(status().isOk())
-        .andExpect(
-            jsonPath("$.username").value("johndoe"))
-        .andExpect(jsonPath("$.email").value("johndoe@email.com"));
+        mockMvc.perform(post("/profile/")
+                .contentType("application/json")
+                .content(asJsonString(postProfile)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("johndoe"))
+                .andExpect(jsonPath("$.email").value("johndoe@email.com"));
 
-    mockMvc.perform(put("/profile/2")
-        .contentType("application/json")
-        .content("{\"username\":\"janedoe\",\"email\":\"janedoe@email.com"
-            + "\"}"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.username").value("janedoe"))
-        .andExpect(jsonPath("$.email").value("janedoe@email.com"));
+        mockMvc.perform(get("/profile/2"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        jsonPath("$.username").value("johndoe"))
+                .andExpect(jsonPath("$.email").value("johndoe@email.com"));
 
-    mockMvc.perform(delete("/profile/2"))
-        .andExpect(status().isOk());
+        Profile putProfile = new Profile("janedoe", "janedoe@email.com");
 
-    assertThrows(
-        Exception.class,
-        () -> mockMvc.perform(get("/profile/2"))
-            .andExpect(status().isNotFound()));
-  }
+        String requestJson = asJsonString(putProfile);
+
+        mockMvc.perform(put("/profile/2")
+                .contentType("application/json")
+                .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("janedoe"))
+                .andExpect(jsonPath("$.email").value("janedoe@email.com"));
+
+        mockMvc.perform(delete("/profile/2"))
+                .andExpect(status().isOk());
+
+        assertThrows(
+                Exception.class,
+                () -> mockMvc.perform(get("/profile/2"))
+                        .andExpect(status().isNotFound()));
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
